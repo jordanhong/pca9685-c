f2265b89b7b0a5c7d07d3b4fab7182dbbcb71246
Jordan Hong <jordanhong89@gmail.com>
2020-07-02 18:38:45 -0400

    Tested output I2C packet

---
+++ Testing/UserInput-Output/compile.sh
@@ -0,0 +1,1 @@
+rm -f ./a.out && gcc -Wall -pedantic $1 && valgrind ./a.out
--- Testing/UserInput-Output/writeFile.c
+++ Testing/UserInput-Output/writeFile.c
@@ -1,17 +1,54 @@
 #include <stdio.h>
+#include <stdint.h>
+int check_fileExists(const char * filename);
 int write2file(const char* path, const char * mode, int* Arr, int ArrLen);
+int writeCommand_2file(const char* path, uint8_t* report, int report_len );
 
 int main (){
-    const char* path = "dataOut.txt";
+    const char* path = "proxy_MCP.txt";
     const char* mode = "w";
-    
-    // Sample data
-    int arrLen = 10;
-    int arrData[10] = {1, 2, 3, 4, 5, 10, 9, 8, 7, 6};
-
-    // call function to write to data
-    write2file(path, mode, arrData, arrLen);
+    uint8_t report[5] = {0xF5, 0x04, 29, 0x01, 0x00};
+    writeCommand_2file(path, report, 5);
     return 0;
+}
+int check_fileExists(const char * filename){
+   /*
+     * Check if a file exist using fopen() function
+     * return 1 if the file exist otherwise return 0
+     */
+
+    /* try to open file to read */
+    FILE *file;
+    if (file = fopen(filename, "r")){
+        fclose(file);
+        return 1;
+    }
+    return 0;
+}
+int writeCommand_2file(const char* path, uint8_t* report, int report_len ){
+    /* report is the I2C package: (uint8_t) [commandMode, len_low_byte, len_high_byte, slave address, data]
+     */
+    FILE* fp = NULL;
+    int fileExists = -1;
+   
+
+    /* If file doesn't exist, create one with headers */
+    fileExists = check_fileExists(path);
+    if (!fileExists){
+       fp = fopen(path, "w");
+       fprintf (fp, "Proxy for I2C Write\n");
+       fprintf (fp, "Command Mode / len LOW BYTE / len HIGH BYTE / SLAVE addr / DATA \n");
+       fclose(fp);
+    }
+
+    /* open the file for appending*/
+    fp = fopen (path, "a");
+    fprintf (fp, "0x%x / 0x%x / 0x%x / 0x%x / 0x%x\n", report[0], report[1], report [2], report[3], report[4] );
+  
+    /* close the file*/  
+    fclose (fp);
+    return 0;
+
 }
 
 
